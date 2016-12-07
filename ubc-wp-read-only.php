0c265639652f21206455af864d6ef77de1e51d04
Richard Tape <richard@iamfriendly.com>
2016-12-07 11:06:12 -0800

    ADDED checks for login, signup, password reset ADDED disabling of AJAX (uuuugly) ADDED disabling of plugin/theme update checks ADDED disabling of heartbeat ADDED better default comment message if no comments template in theme ADDED attributions of previous art

--- ubc-wp-read-only.php
+++ ubc-wp-read-only.php
@@ -5,13 +5,20 @@
  * @wordpress-plugin
  * Plugin Name:	   UBC WP Read Only
  * Plugin URI:		http://ctlt.ubc.ca/
- * Description:	   Puts a site into read-only mode to make migrating servers easier
+ * Description:	   Puts a site into read-only mode to make migrating servers easier. Be signed in as a Super Admin before activating.
  * Version:		   0.1.0
  * Author:			Richard Tape
  * Author URI:		http://blogs.ubc.ca/richardtape
  * License:		   GPL-2.0+
  * License URI:	   http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:	   ubc-wp-read-only
+*/
+
+/*
+	Previous art, with thanks:
+	force-reauthentication
+	code-freeze
+	wp-schedules-read-only
 */
 
 namespace UBC;
@@ -110,10 +117,28 @@ class WP_Read_Only {
 		add_action( 'admin_init', array( __CLASS__, 'init__log_out_non_superadmins' ) );
 
 		// Disable cron if it's not already done
-		add_action( 'plugins_loaded', array( __CLASS__, 'plugins_loaded__disable_cron' ) );
+		add_action( 'plugins_loaded', array( __CLASS__, 'plugins_loaded__disable_cron' ), 1 );
+
+		// Disable update checks, should be covered by disabling cron, but better safe than sorry
+		add_action( 'site_transient_update_plugins', '__return_false' );
+		add_action( 'site_transient_update_themes', '__return_false' );
 
 		// Hijack adding an option. Not pretty.
 		add_action( 'add_option', array( __CLASS__, 'add_option__hijack_my_name_is_not_jack' ), 1, 2 );
+
+		// Prevent heartbeat.
+		add_action( 'init', array( __CLASS__, 'init__disable_heartbeat' ), 1 );
+
+		// Kill the wp-login.php file
+		add_action( 'login_enqueue_scripts', array( __CLASS__, 'login_enqueue_scripts__kill_login_php' ), 1 );
+
+		// Kill the wp-signup.php file
+		add_action( 'before_signup_header', array( __CLASS__, 'before_signup_header__kill_signup' ), 1 );
+
+		// Kill password resets, lost pw etc.
+		add_action( 'lost_password', array( __CLASS__, 'generic__kill_switch' ) );
+		add_action( 'retrieve_password', array( __CLASS__, 'generic__kill_switch' ) );
+		add_action( 'password_reset', array( __CLASS__, 'generic__kill_switch' ) );
 
 	}/* add_actions() */
 
@@ -133,12 +158,21 @@ class WP_Read_Only {
 		add_filter( 'comments_open', '__return_false', 999, 2 );
 		add_filter( 'pings_open', '__return_false', 999, 2 );
 
+		// Change message from 'comments are closed' to be more useful, only useful when no comments.php file in theme
+		add_filter( 'comment_form_defaults', array( __CLASS__, 'comment_form_defaults__change_comments_closed_message' ), 999, 1 );
+
 		// For bbPress, no forms for you
 		add_filter( 'bbp_current_user_can_access_create_topic_form', '__return_false' );
 		add_filter( 'bbp_current_user_can_access_create_reply_form', '__return_false' );
 
 		// Ensure no options can be updated
 		add_filter( 'pre_update_option', array( __CLASS__, 'pre_update_option__disable_options_updates' ), 999, 3 );
+
+		// Disable AJAX, pre WP 4.7
+		add_action( 'admin_init', array( __CLASS__, 'admin_init__disable_ajax' ) );
+
+		// 4.7+ Only. Disable AJAX. Other AJAX killing methods can be removed when 4.7+ is min. req. for this plugin.
+		add_filter( 'wp_doing_ajax', array( __CLASS__, 'wp_doing_ajax__disable_ajax' ) );
 
 	}/* add_filters() */
 
@@ -169,6 +203,25 @@ class WP_Read_Only {
 		return $posts;
 
 	}/* the_posts__disable() */
+
+
+	/**
+	 * WP, by default, has a 'comments are closed' message. We should be a little more discriptive
+	 * here to say they're temporarily closed.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (array) The default comment form arguments.
+	 * @return null
+	 */
+
+	public static function comment_form_defaults__change_comments_closed_message( $defaults ) {
+
+		$defaults['title'] = __( 'Comments are temporarily closed because this site is in read-only mode.', 'ubc-wp-read-only' );
+
+		return $defaults;
+
+	}/* comment_form_defaults__change_comments_closed_message() */
 
 
 	/**
@@ -238,6 +291,22 @@ class WP_Read_Only {
 
 
 	/**
+	 * Disable WP heartbeat
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param null
+	 * @return null
+	 */
+
+	public static function init__disable_heartbeat() {
+
+		wp_deregister_script( 'heartbeat' );
+
+	}/* init__disable_heartbeat() */
+
+
+	/**
 	 * I shouldn't be allowed to write code. I am sorry.
 	 *
 	 * If add_option() is called, we need to make sure no db writes get attempted.
@@ -257,9 +326,100 @@ class WP_Read_Only {
 
 	public static function add_option__hijack_my_name_is_not_jack( $option, $value ) {
 
-		wp_die( __( 'This site is currently in read-only mode. Updates are currently prohibited.' . print_r( array( $option, value ), true ), 'ubc-wp-read-only' ) );
+		wp_die( esc_html__( 'This site is currently in read-only mode. Updates are currently prohibited.', 'ubc-wp-read-only' ) );
 
 	}/* add_option__hijack_my_name_is_not_jack() */
+
+
+	/**
+	 * Kill the wp-login.php early in the process, preventing direct access to the file
+	 * or redirects to it.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param null
+	 * @return null
+	 */
+
+	public static function login_enqueue_scripts__kill_login_php() {
+
+		?>
+			</head>
+		</html>
+
+		<?php
+
+		wp_die( esc_html__( 'This site is currently in read-only mode. Log in is currently prohibited.' ) );
+
+	}/* login_enqueue_scripts__kill_login_php() */
+
+
+	/**
+	 * Kill direct access to wp-signup.php
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param null
+	 * @return null
+	 */
+
+	public static function before_signup_header__kill_signup() {
+
+		wp_die( esc_html__( 'This site is currently in read-only mode. Sign up is currently prohibited.' ) );
+
+	}/* before_signup_header__kill_signup() */
+
+
+	/**
+	 * A generic method to show a wp_die() message.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param null
+	 * @return null
+	 */
+
+	public static function generic__kill_switch() {
+
+		wp_die( esc_html__( 'This site is currently in read-only mode.' ) );
+
+	}/* generic__kill_switch() */
+
+
+	/**
+	 * Hook in as early as we can, determine if we are DOING_AJAX, and if so, kill it
+	 * ~with fire~.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param null
+	 * @return null
+	 */
+
+	public static function admin_init__disable_ajax() {
+
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+			die( esc_html__( 'This site is currently in read-only mode.' ) );
+		}
+
+	}/* admin_init__disable_ajax() */
+
+
+	/**
+	 * Kill switch for when an AJAX request is being made.
+	 * Requires WordPress 4.7.0+
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param (bool) $doing_ajax - Whether we're currently performing an AJAX request
+	 * @return null
+	 */
+
+	public static function wp_doing_ajax__disable_ajax( $doing_ajax ) {
+
+		die( esc_html__( 'This site is currently in read-only mode.' ) );
+
+	}/* wp_doing_ajax__disable_ajax() */
 
 
 	/**
